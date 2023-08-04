c0daa8e40c56ee440cbdf452cc8a47d05b20ad31
ronaldodeschain <lotushall@gmail.com>
2023-08-04 14:10:59 -0300

    corringindo nomenclatura do metodo de salario alto

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.alura.tdd.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -12,11 +13,12 @@ import br.com.alura.tdd.modelo.Funcionario;
 class BonusServiceTest {
 
 	@Test
-	void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+	void bonusDeveriaLancarExcecaoParaFuncionarioComSalarioMuitoAlto() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+		assertThrows(IllegalArgumentException.class,
+				() -> service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000"))));
 
-		assertEquals(new BigDecimal("0.00"), bonus);
+		
 	}
 
 	@Test
